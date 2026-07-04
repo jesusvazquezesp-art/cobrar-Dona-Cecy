0fe4f357835d002aacd8fddbcedad6c6531eb432
jesusvazquezesp-art <jesusvazquezesp@hotmail.com>
2026-07-04 06:08:46 -0600

    Upgrade Firebase SDK to version 10.13.2
    Updated Firebase SDK versions in service worker.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 /* Firebase Messaging Service Worker - Punto de Ventas Doña Cecy V11.1 */
-importScripts('https://www.gstatic.com/firebasejs/12.15.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/12.15.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyCdKLeXKOTStj0nXy2CgcTA_K1GC7k8gBE",
