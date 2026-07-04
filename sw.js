6bcc2f125e308c5899dc48451e4baefeae48868b
jesusvazquezesp-art <jesusvazquezesp@hotmail.com>
2026-07-03 22:00:48 -0600

    Simplify event listener registration in sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,3 @@
-self.addEventListener("install", event => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener("install", event => { self.skipWaiting(); });
+self.addEventListener("activate", event => { event.waitUntil(clients.claim()); });
+self.addEventListener("fetch", event => { event.respondWith(fetch(event.request)); });
